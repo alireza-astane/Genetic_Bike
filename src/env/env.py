a2f8be2fa73c432cbd59f204d716557a4da7daab
alireza-astane <apoorhass@gmail.com>
2024-11-26 18:33:43 +0330

    add runge kutta 4 method

--- src/env/env.py
+++ src/env/env.py
@@ -1,32 +1,83 @@
-# this is just an example of how we can write the abstract of the class and may change later !!!!!
+# what should the trajectory look like?
+
+
 class env:
     def __init__(self):
-        # constructor
         self.trajectory = []
         self.n_bikes = 0
+        self.time = 0  # time in seconds
+        self.time_step = 1  # time step in seconds
+        self.bikes = []
+        self.starting_positions = np.zeros((self.n_bikes, 2))
+        self.ground_derivative = None
+        self.ground = None
 
-    def step(self):
-        # step function
-        pass
-
-    def add_bikes(self, list_bikes):
-        # add bikes
-        pass
+    def set_bikes(self, list_bikes):
+        self.bikes = list_bikes
 
     def get_trajectory(self):
         # maybe someone wants to get the trajectory to visualize it
         return self.trajectory
 
-    def evaluate(self):
-        # evaluate the environment
+    def evaluate(self, R0, R):
 
-        scores = np.zeros(self.n_bikes)
+        # evaluate the current state of the environment
+
+        # compare last element of trajectory with the starting positions
+
+        scores = R[:, 0] = R0[:, 0]
         return scores
 
+    def run(self, n):
+        trajectory = np.zeros((n, self.n_bikes, 2))
+        # run the environment
+        self.time = 0
+        R, V, t = ...
+        R0 = R
+        for i in range(n):
+            R, V, t = self.step(R, V, t, self.calculate_acceleration)
+            self.time += self.time_step
+            trajectory[i, :, :] = R
 
-# just wrtie the abstract of the class and fields and methods
-# and then write the implementation afterward
-# others will know which functions to call, what to input and what to expect from the class
-# this is a good way to write code for collaboration
-# I think we should write the abstracts frist, then we check with each other and then write the implementation and complete the empty functions
-# this way we can divide the work and also know what to expect from each other
+        scores = self.evaluate(R0, R)
+
+        return trajectory, scores
+
+    def calculate_forces(self, bike):
+        # calculate the forces acting on the bike based on the trajectory and env    #### ???????/
+        pass
+
+    def calculate_acceleration(self, bike):
+        # calculate the acceleration of the bike
+        pass
+
+    def create_ground(self):
+        # example
+        self.ground = np.sin
+        self.ground_derivative = np.cos
+
+    def step(self, R, V, t, acceleration_function):
+        # ronge kutta method for solving the differential equation
+
+        k1 = self.time_step * acceleration_function(V, R, t)
+        theta1 = V
+
+        k2 = self.time_step * acceleration_function(
+            V + k1 / 2, R + theta1 / 2, t + self.time_step / 2
+        )
+        theta2 = V + k1 / 2
+
+        k3 = self.time_step * acceleration_function(
+            V + k2 / 2, R + theta2 / 2, t + self.time_step / 2
+        )
+        theta3 = V + k2 / 2
+
+        k4 = self.time_step * acceleration_function(
+            V + k3, R + theta3, t + self.time_step
+        )
+        theta4 = V + k3
+
+        V = V + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        R = R + (theta1 + 2 * theta2 + 2 * theta3 + theta4) / 6
+
+        return R, V, t
