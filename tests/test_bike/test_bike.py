bab45c66e7d311c82bdd8c08e5a0128e9f4bf597
Aref Ghorbani <arefpj@gmail.com>
2024-11-27 17:33:46 +0100

    Issue#2_test: test added.

---
+++ tests/test_bike/test_bike.py
@@ -0,0 +1,10 @@
+#import pytest
+from src.bike.bike import Bike
+
+def test_get_get_masses():
+    test_bike = Bike(
+        0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0)
+    expectes_res = [0,0,0,0]
+    assert(test_bike.get_masses() == expectes_res)
