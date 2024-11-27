6db069aeacf84cbb1fcaaaf37b2237083044a6ac
alireza-astane <apoorhass@gmail.com>
2024-11-27 16:14:51 +0330

    change ground function and dynamic

--- src/env/env.py
+++ src/env/env.py
@@ -1,4 +1,5 @@
 # what should the trajectory look like?
+import numpy as np
 
 
 class env:
@@ -9,9 +10,16 @@ class env:
         self.time_step = 1  # time step in seconds
         self.bikes = []
         self.starting_positions = np.array([0, 10])  # ???
-        self.ground_derivative = None
-        self.ground = None
-        self.ms = np.zeros((self.n_bikes, r))
+        self.x_max = 1000
+        self.delta_x = 0.001
+        self.ground_derivative = 1  ## examplle
+        self.ground = np.array(
+            [
+                np.linspace(0, self.x_max, int(self.x_max / self.delta_x)),
+                np.linspace(0, self.x_max, int(self.x_max / self.delta_x)),
+            ]
+        ).T  # ground function
+        self.ms = np.zeros((self.n_bikes, 4))
         self.g = -g * np.array([0, 1])
 
     def set_bikes(self, list_bikes):
@@ -60,11 +68,13 @@ class env:
 
         return lengths
 
+    def get_Radius(self):
+        radisuss = np.zeros((self.n_bikes, 2))
+        for i in range(self.n_bikes):
+            radisuss[i] = self.bikes[i].get_radius()
+
     def get_trajectory(self):
         return self.trajectory
-
-    def evaluate(self):
-        return self.R[:, 0] - self.R0[:, 0]
 
     def run(self, n):
         self.trajectory = np.zeros((n, self.n_bikes, 2))
@@ -85,12 +95,6 @@ class env:
         self.scores = self.evaluate(R0, R)
 
         return self.trajectory, self.scores
-
-    def create_ground(self, func=np.sin, derivative=np.cos):
-        self.ground = func
-        self.ground_derivative = derivative
-
-        return self.ground, self.ground_derivative
 
     def step(self):
         # ronge kutta method for solving the differential equation
@@ -117,6 +121,40 @@ class env:
         R = R + (theta1 + 2 * theta2 + 2 * theta3 + theta4) / 6
 
         return R, V, t
+
+        def calculate_forces(self, R, V, t):
+            # calculate the forces acting on the bike based on the trajectory and env    #### ???????/
+            W = self.ms * self.g  # shapes = (n_bikes * 4) * 2 = n_bikes * 4 * 2
+            damping_force = (
+                -self.B @ V
+            )  # shape = (n_bikes * 4 * 4 ) @ (n_bikes * 4 * 2 ) = n_bikes * 4 * 2
+            initial_lengths = self.init_lengths @ self.get_distance_unit_vector(
+                R
+            )  # shape = (n_bikes * 4 * 4) @ (n_bikes * 4 * 2) = n_bikes * 4 * 2
+            spring_force = -self.K @ (
+                R - self.initial_lengths
+            )  # shape = (n_bikes * 4 * 4) @ (n_bikes * 4 * 2) = n_bikes * 4 * 2
+            n_hat = self.perpendicular_unit_vector(R)  # shape = n_bikes * 4 * 2
+            t_hat = self.parallel_unit_vector(R)  # shape = n_bikes * 4 * 2
+            C = self.get_connected(R)  # shape = n_bikes * 4
+
+            turk_force = (
+                self.torks * C @ t_hat
+            )  # shape = (n_bikes * 4) * (n_bikes * 4) * (n_bikes * 4 * 2) = n_bikes * 4 * 2   ?????
+
+            # M * X_vec.. = - K (X_vec - l0 * d_hat) - B * V_vec + W_vec + tork * T * C + N * C
+
+            Force = W + damping_force + spring_force + turk_force
+            Force -= (
+                np.dot(Force, n_hat, axis=...) * n_hat
+            )  # remove the perpendicular component
+            return Force
+
+    def create_ground(self, func=np.sin, derivative=np.cos):
+        self.ground = func
+        self.ground_derivative = derivative
+
+        return self.ground, self.ground_derivative
 
     def calculate_acceleration(self, R, V, t):
         return self.calculate_forces(R, V, t) / self.ms
@@ -158,63 +196,62 @@ class env:
         distance = R - np.traspose(...)
         normalized_distance = distance / np.linalg.norm(distance, axis=2)
         # put zeros for diagnal elements
-        normalized_distance[range(self.n_bikes), range(self.n_bikes)] = 0  # ?????
+        normalized_distance[range(4), range(4)] = 0  # ?????
         return normalized_distance
 
-    def calculate_forces(self, R, V, t):
-        # calculate the forces acting on the bike based on the trajectory and env    #### ???????/
-        pass
-        W = self.ms * self.g  # shapes = (n_bikes * 4) * 2 = n_bikes * 4 * 2
-        damping_force = (
-            -self.B @ V
-        )  # shape = (n_bikes * 4 * 4 ) @ (n_bikes * 4 * 2 ) = n_bikes * 4 * 2
-        initial_lengths = self.init_lengths @ self.get_distance_unit_vector(
-            R
-        )  # shape = (n_bikes * 4 * 4) @ (n_bikes * 4 * 2) = n_bikes * 4 * 2
-        spring_force = -self.K @ (
-            R - self.initial_lengths
-        )  # shape = (n_bikes * 4 * 4) @ (n_bikes * 4 * 2) = n_bikes * 4 * 2
-        n_hat = self.perpendicular_unit_vector(R)  # shape = n_bikes * 4 * 2
-        t_hat = self.parallel_unit_vector(R)  # shape = n_bikes * 4 * 2
-        C = self.get_connected(R)  # shape = n_bikes * 4
-
-        turk_force = (
-            self.torks * C @ t_hat
-        )  # shape = (n_bikes * 4) * (n_bikes * 4) * (n_bikes * 4 * 2) = n_bikes * 4 * 2   ?????
-
-        # M * X_vec.. = - K (X_vec - l0 * d_hat) - B * V_vec + W_vec + tork * T * C + N * C
-
-        Force = W + damping_force + spring_force + turk_force
-        Force -= (
-            np.dot(Force, n_hat, axis=...) * n_hat
-        )  # remove the perpendicular component
-        return Force
-
     def get_connected(self, R):
+        radiuses = self.get_Radius()
+
         C = np.zeros((self.n_bikes, 4), dtype=bool)
         C[:, 0] = self.ground(R[:, 0, 0]) > R[:, 0, 1]
         C[:, 1] = self.ground(R[:, 1, 0]) > R[:, 1, 1]
         distance_c = self.calculate_distance_from_ground(R[:, 2])
         distance_d = self.calculate_distance_from_ground(R[:, 3])
 
-        C[:, 2] = distance_c == 0  # or almost zero ????
-        C[:, 3] = distance_d == 0  # or almost zero ???/
+        C[:, 2] = (
+            np.abs(distance_c - radiuses[:, 0]) < 0e-5
+        )  # or almost zero ????   it should be compared to the radius of the wheel
+        C[:, 3] = (
+            np.abs(distance_d - radiuses[:, 1]) < 0e-5
+        )  # or almost zero ???/ it should be compared to the radius of the wheel
 
         return C
 
-    def calculate_distance_from_ground(self, pos):
-        a = pos[:, 0]
-        b = -self.ground_derivative(pos[:, 0]) * (pos[:, 1] - self.ground(pos[:, 0]))
+    # tested functions
 
-        min_xs = np.min(
-            np.array([a / 2 + np.sqrt(a**2 / 4 + b), a / 2 - np.sqrt(a**2 / 4 + b)]),
-            axis=1,
+    def evaluate(self):
+        return self.R[:, 0] - self.R0[:, 0]
+
+    def calculate_distance_from_ground(self, pos):
+
+        return np.min(
+            np.linalg.norm(
+                np.tile(self.ground, (3, 1, 1)).transpose((1, 0, 2)) - pos, axis=2
+            ),
+            axis=0,
         )
 
-        return self.get_distance_from_x(pos, min_xs)
 
-    def get_distance_from_x(self, pos, x):
-        return np.sqrt((pos[:, 0] - x) ** 2 + (pos[:, 1] + self.groun(x)) ** 2)
+# tests
 
 
-### make sum tests and run the code
+def test_calculate_distance_from_ground():
+    test_env = env(9.8)
+    pos = np.array([[1, 1], [2, 4], [3, 9]])
+    assert np.all(
+        test_env.calculate_distance_from_ground(pos)
+        - np.array([0, np.sqrt(2), 3 * np.sqrt(2)])
+        < 1e-5
+    )
+
+
+def test_evalute():
+    test_env = env(9.8)
+    test_env.R = np.array([[[0, 0], [0, 0], [0, 0], [0, 0]]])
+    test_env.R0 = np.array([[[0, 0], [0, 0], [0, 0], [0, 0]]])
+    assert np.all(test_env.evaluate() == np.array([[[0, 0], [0, 0], [0, 0], [0, 0]]]))
+
+
+test_evalute()
+
+test_calculate_distance_from_ground()
